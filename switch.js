788b43e571697f17279e415241c3797f8aa81f5a
imahacs <mem880.a@gmail.com>
2024-04-08 00:57:40 +0300

    Add sound, fotter links

---
+++ switch.js
@@ -0,0 +1,45 @@
+
+// Player(play/pause/stop) using JavaScript:
+
+// var audio = document.getElementById('audio');
+// var playpause = document.getElementById('playpause');
+// var count = 0;
+
+
+// function playPause() {
+//   if (count == 0) {
+//     count = 1;
+//     audio.play();
+//     playpause.innerHTML = "Play &#9208;";
+
+//   }
+//   else {
+//     count = 0;
+//     audio.pause();
+//     playpause.innerHTML = "Play &#9658;";
+//   }
+// }
+
+// function stop() {
+//   playPause()
+//   audio.pause();
+//   audio.currentTime = 0;
+//   playpause.innerHTML = "Play &#9658;";
+// }
+//ending of player
+
+
+//Triggering the player using checkbox:
+var audio = document.getElementById('audio');
+$('input[type="checkbox"]').click(function () {
+  if ($(this).prop("checked") == true) {
+    audio.play();
+  }
+  else if ($(this).prop("checked") == false) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+});
+
+
+
